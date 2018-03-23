8b488d56f3b29a3dfbf6c029a76a1a300d37dccf
Ash Taylor <a@zi.vc>
2018-03-23 19:30:09 +0000

    fucking shitpost mode

--- src/commands/Trigger.ts
+++ src/commands/Trigger.ts
@@ -3,13 +3,61 @@ import { Pool } from 'mysql2/promise';
 import BotData from '../services/Data';
 import Logger from '../services/Logger';
 import _ = require('lodash');
+import Message from '../slack-types/Message';
+import Daemon from '../services/Daemon';
 
 export default class Trigger extends Command {
 
-    private triggerChanceToSpeak: number = 20;
-    private triggerReplyInterval: number = 1000 * 60 * 10;
-
     private pool: Pool = BotData.getPool();
+    private currentlyShitposting: boolean = false;
+    private triggerReplyInterval: number = 1000 * 60 * 10;
+    
+    private async broadcastShitpost(enabled: boolean) {
+        const [channels] = await this.pool.query('SELECT `id`, `team` FROM `channels`') as any;
+        channels.forEach((channel: any) => {
+            const install = Daemon.getInstall(channel.team);
+            if (!install) return;
+            const obj = Message.installObj(install);
+            obj.install.rtm.sendMessage(
+                (enabled) ?
+                    ':shitposting: shitposting has begun. happy friday xxx :shitposting:' :
+                    'Shitposting has ended. Time to send the dealer a text!',
+                channel.id
+            );
+        })
+    }
+
+    private get triggerChanceToSpeak() {
+        // These are the inverse of 100, 95 = 5%, 20 = 80%
+        const chanceToSpeak = 95;
+        const shitpostFriday = 20;
+
+        // subtract one because javascript dates 0-23
+        const startTimeHours = 9 - 1;
+        const endTimeHours = 18 - 1;
+        const hectorsOffset = -3; // hours
+
+        const now = new Date(+new Date + (hectorsOffset * 3600 * 1000));
+        const currentHour = now.getHours();
+
+        // Has to be friday
+        if (now.getDay() !== 5) return chanceToSpeak;
+
+        if (currentHour >= startTimeHours && currentHour <= endTimeHours) {
+            if (!this.currentlyShitposting) {
+                this.currentlyShitposting = true;
+                this.broadcastShitpost(true);
+            }
+            return shitpostFriday;
+        }
+
+        if (this.currentlyShitposting) {
+            this.currentlyShitposting = false;
+            this.broadcastShitpost(false);
+        }
+        return chanceToSpeak;
+
+    }
 
     constructor() {
         super('trigger', ['t', 'trigger']);
@@ -22,7 +70,7 @@ export default class Trigger extends Command {
 
     async reply(message: any, obj: any) {
 
-        const { action, params } = this.parseText(message.text);
+        const {action, params} = this.parseText(message.text);
 
         const actions = ['add']; //'delete'];
 
@@ -38,11 +86,11 @@ export default class Trigger extends Command {
     async addAction(message: any, params: any, obj: any) {
         const trigger = params.split(' ', 1)[0];
         const phrase = params.substr(trigger.length).trim();
-        const { user } = message;
+        const {user} = message;
         const team = obj.teamId;
         if (!trigger || !phrase || !user) return '';
 
-        const insert = await this.pool.query('INSERT INTO `triggers` SET ?', { trigger, phrase, user, team }) as any;
+        const insert = await this.pool.query('INSERT INTO `triggers` SET ?', {trigger, phrase, user, team}) as any;
 
         if (insert.length) {
             return `Added "${phrase}" as a response to *${trigger}?*`;
@@ -60,7 +108,7 @@ export default class Trigger extends Command {
         // const [chanceToSpeak] = await this.pool.query('SELECT `chancetospeak` FROM `installs` WHERE `team` = ?', [obj.teamId]) as any;
         // this.triggerChanceToSpeak = 100 - chanceToSpeak[0];
 
-        const { store } = obj.install;
+        const {store} = obj.install;
 
         const lastSpoke = +new Date() - store.lastTrigger;
         const dice = Math.floor(Math.random() * 100) + 1;
@@ -73,7 +121,7 @@ export default class Trigger extends Command {
         if (!doReply) return '';
 
         const [data] = await this.pool.query('SELECT `trigger` FROM `triggers` WHERE team = ? GROUP BY `trigger`', [obj.teamId]) as any;
-        const keywords = data.map((row:any) => row.trigger);
+        const keywords = data.map((row: any) => row.trigger);
 
         const text = message.text.toLowerCase();
         const triggers = _.shuffle(text.split(' '));
@@ -86,7 +134,7 @@ export default class Trigger extends Command {
             if (trigger && keywords.indexOf(trigger) !== -1) {
 
                 const [responses] = await this.pool.query('SELECT `phrase` FROM `triggers` WHERE `trigger` = ? AND team = ?', [trigger, obj.teamId]) as any;
-                const phrases = responses.map((row:any) => row.phrase);
+                const phrases = responses.map((row: any) => row.phrase);
 
                 const phrase = _.shuffle(phrases)[0].toString();
 
